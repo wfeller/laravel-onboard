2c144b2bb57a80d4ce6d40f78f748827e86f9eb1
william <billyfeller@gmail.com>
2019-01-17 16:16:08 +0100

    facade type hinting

--- src/OnboardFacade.php
+++ src/OnboardFacade.php
@@ -5,6 +5,9 @@ namespace WF\Onboard;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \WF\Onboard\OnboardingStep addStep(string $title, string|array $userClass = null)
+ * @method static \Illuminate\Support\Collection|\WF\Onboard\OnboardingStep[] steps(object $user)
+ *
  * @see \WF\Onboard\OnboardingSteps
  */
 class OnboardFacade extends Facade
